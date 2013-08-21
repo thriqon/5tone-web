ea51796cc903b51719106b96fc4f113899bc7ead
Jonas Weber <mail@jonasw.de>
2013-08-21 13:01:51 -0400

    errors caught and warning display

--- app.js
+++ app.js
@@ -1,85 +1,90 @@
-
-function ZVEIPlayer() {
-	this.context = new webkitAudioContext();
-}
-
-function toZveiFrequencies (digits) {
-	if (!digits || digits.length === 0) {
-		return [];
-	}
-
-	var freqs = {
-		'1' : 1060,
-		'2' : 1160,
-		'3' : 1270,
-		'4' : 1400,
-		'5' : 1530,
-		'6' : 1670,
-		'7' : 1830,
-		'8' : 2000,
-		'9' : 2200,
-		'0' : 2400,
-		'R' : 2600
-	};
-	
-	var res = [], i;
-	res[0] = freqs[digits[0]];
-
-	for (i = 1; i < digits.length; i++) {
-		res[i] = freqs[digits[i]];
-
-		if (res[i - 1] === res[i]) {
-			res[i] = freqs.R;
-		}
-	}
-
-	return res;
-}
-
-ZVEIPlayer.prototype.play = function (digits) {
-
-	var osc = this.context.createOscillator();
-	osc.connect(this.context.destination);
-
-	var startTime = this.context.currentTime;
-	osc.start(startTime);
-
-	toZveiFrequencies(digits).forEach(function (freq) {
-		osc.frequency.setValueAtTime(freq, startTime);
-		startTime += 0.07;
-	});
-
-	osc.stop(startTime);
-/*	var oscillators = [];
-	startTime = this.context.currentTime;
-
-	toZveiFrequencies(digits).forEach(function (freq) {
-		var osc = this.context.createOscillator();
-		osc.type = 0;
-		osc.frequency.value = freq;
-		osc.connect(this.context.destination);
-		osc.start(startTime);
-		console.log('OSC ' + startTime);
-		startTime += 0.07;
-		osc.stop(startTime);
-	}.bind(this));*/
-};
+$(function() {
+                if (!init()) {
+                    return;
+                }
+                $('body').removeClass('nojs');
+                
 
 
-ZVEIPlayer.prototype.alarm = function (schleife, finishcb) {
-	var self = this;
+                var ddigitsSpans = $('.ddigit');
+                var alarmButton = $('.alarm');
+                var digitsButtons = $('.digit');
+                var loeschenButton = $('.loeschen');
 
-	setTimeout(function () {
-		self.play(schleife);
-		setTimeout(function () {
-			self.play(schleife);
-            
-            setTimeout(finishcb, 70 * schleife.length + 600);
-            
-		}, 70 * schleife.length + 600);
-	}, 600);
-};
+                var digits = [-1, - 1, - 1, - 1, - 1];
 
-function init () {
-	window.zveigen = new ZVEIPlayer();
-}
+                function refreshDigits() {
+                    ddigitsSpans.each(function(index) {
+
+                        if (digits[index] == -1) {
+                            $(this).text('-').addClass('bg');
+                        }
+                        else {
+                            $(this).text(digits[index]).removeClass('bg');
+                        }
+                    });
+
+                    // alle Digits gesetzt
+                    if (digits.indexOf(-1) == -1) {
+                        alarmButton.removeAttr('disabled');
+                        digitsButtons.attr('disabled', 'disabled');
+                        loeschenButton.removeAttr('disabled');
+
+                        // kein Digit gesetzt
+                    }
+                    else if (digits.indexOf(-1) == 0) {
+                        alarmButton.attr('disabled', 'disabled');
+                        digitsButtons.removeAttr('disabled');
+                        loeschenButton.attr('disabled', 'disabled');
+                    }
+                    else {
+                        // irgendwo zwischendrin, es gibt ungesetzte Digits, aber auch gesetzte
+                        alarmButton.attr('disabled', 'disabled');
+                        digitsButtons.removeAttr('disabled');
+                        loeschenButton.removeAttr('disabled');
+                    }
+                }
+
+                digitsButtons.click(function() {
+                    var index = digits.indexOf(-1);
+                    if (index == -1) {
+                        console.log('ouch');
+                        return;
+                    }
+
+                    digits[index] = parseInt($(this).text());
+
+                    refreshDigits();
+                });
+
+                alarmButton.click(function() {
+                    $('.alarmgeber').addClass('active');
+                    loeschenButton.attr('disabled', 'disabled');
+                    zveigen.alarm(digits, function() {
+                        $('.alarmgeber').removeClass('active');
+
+                        for (var i = 0; i < digits.length; i++) {
+                            digits[i] = -1;
+                        }
+                        refreshDigits();
+                    });
+                });
+
+                loeschenButton.click(function() {
+                    var indexP = digits.indexOf(-1);
+                    if (indexP == 0) {
+                        return;
+                    }
+                    if (indexP == -1) {
+                        indexP = digits.length;
+                    }
+
+                    var index = indexP - 1;
+
+                    digits[index] = -1;
+
+                    refreshDigits();
+                });
+
+                refreshDigits();
+            });
